34585c18cd25a307bc2d58b58484503e228d177a
jean de la paix <104870892+lapaix123@users.noreply.github.com>
2023-09-13 16:25:41 +0200

    uploading my backend

---
+++ employeManager/src/main/java/lapaix/it/employeManager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package lapaix.it.employeManager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ employeManager/src/main/java/lapaix/it/employeManager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package lapaix.it.employeManager.repo;
+
+import lapaix.it.employeManager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
